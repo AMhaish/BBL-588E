e6d27e29d4f60ce94c6c2898500a07d6d09c7893
Ahmad Mhaish <a.mhaish@gmail.com>
2020-06-14 21:19:59 +0300

    Working on homework2

---
+++ HW2/main.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+from sklearn import cluster
+from sklearn.datasets.samples_generator import make_blobs
+
+
+def doKmeansCluster(image, numberOfClusters=3):
+    x, y, z = img.shape
+    image_2d = img.reshape(x*y, z)
+    print('Clustering the image using %d clusters' % (numberOfClusters))
+    kmeans_cluster = cluster.KMeans(n_clusters=numberOfClusters)
+    kmeans_cluster.fit(image_2d)
+    cluster_centers = kmeans_cluster.cluster_centers_
+    cluster_labels = kmeans_cluster.labels_
+    resultImage = cluster_centers[cluster_labels].reshape(x, y, z)
+    return resultImage.astype(int)
+
+
+def doMeanShift(image):
+    image_2d = np.reshape(image, [-1, 3])
+    bandwidth = cluster.estimate_bandwidth(
+        image_2d, quantile=0.1, n_samples=100)
+    print(bandwidth)
+    meanShift_cluster = cluster.MeanShift(
+        bandwidth=bandwidth, bin_seeding=True)
+    meanShift_cluster.fit(image_2d)
+    cluster_centers = meanShift_cluster.cluster_centers_
+    cluster_labels = meanShift_cluster.labels_
+    print("number of predicted clusters : %d" % len(np.unique(cluster_labels)))
+    segmented_image = np.reshape(cluster_labels, image.shape[:2])
+    return segmented_image.astype(int)
+
+
+print('Loading image')
+img = mpimg.imread('SunnyLake.bmp')
+
+print('Clustering using KMeans algorithim')
+plt.subplot(231), plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(232), plt.imshow(
+    doKmeansCluster(img, 3)), plt.title('Clustered with K=3')
+plt.xticks([]), plt.yticks([])
+plt.subplot(233), plt.imshow(
+    doKmeansCluster(img, 5)), plt.title('Clustered with K=5')
+plt.xticks([]), plt.yticks([])
+plt.subplot(234), plt.imshow(
+    doKmeansCluster(img, 7)), plt.title('Clustered with K=7')
+plt.xticks([]), plt.yticks([])
+plt.subplot(235), plt.imshow(
+    doKmeansCluster(img, 9)), plt.title('Clustered with K=9')
+plt.xticks([]), plt.yticks([])
+plt.subplot(236), plt.imshow(
+    doKmeansCluster(img, 12)), plt.title('Clustered with K=12')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+print('Clustering using MeanShift algorithim')
+plt.xticks([]), plt.yticks([])
+plt.subplot(121), plt.imshow(
+    img), plt.title('')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(
+    doMeanShift(img)), plt.title('')
+plt.xticks([]), plt.yticks([])
+plt.show()
---
+++ Project/train2.py
@@ -0,0 +1,26 @@
+# import the necessary packages
+from pyimagesearch.features import load_dataset
+from sklearn.ensemble import RandomForestClassifier
+import argparse
+import pickle
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+                help="path to dataset of images")
+ap.add_argument("-m", "--model", required=True,
+                help="path to output anomaly detection model")
+args = vars(ap.parse_args())
+
+# load and quantify our image dataset
+print("[INFO] preparing dataset...")
+data = load_dataset(args["dataset"], bins=(3, 3, 3))
+print(data[1, 1])
+# train the anomaly detection model
+print("[INFO] fitting anomaly detection model...")
+model = RandomForestClassifier(n_jobs=2, random_state=0)
+model.fit(data)
+
+# serialize the anomaly detection model to disk
+f = open(args["model"], "wb")
+f.write(pickle.dumps(model))
+f.close()
